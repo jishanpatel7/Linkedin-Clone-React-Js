e48f4cd2eabefbd41811dc375b2fdd9d0b93ecd0
jishan <jishanpatel78@gmail.com>
2022-02-24 08:25:00 +0530

    commit

--- src/Feed.jsx
+++ src/Feed.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./css/feed.css";
 import { Avatar } from '@mui/material'
 import PhotoIcon from '@mui/icons-material/Photo';
@@ -6,50 +6,74 @@ import TodayIcon from '@mui/icons-material/Today';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import ArticleIcon from '@mui/icons-material/Article';
 import { Post } from './components/Post';
+import { db } from './firebase';
+import firebase from 'firebase/compat/app';
+
+
 export const Feed = () => {
+    const [input, setInput] = React.useState('');
+    const [posts, setPosts] = React.useState([]);
+    const submitPost = (e) => {
+        e.preventDefault();
+        db.collection('posts').add({
+            name: "Jishan Pathan",
+            description: "this is a test post",
+            message: input,
+            photoUrl: "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        })
+        setInput('');
+    }
+
+    useEffect(() => {
+        db.collection('posts').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
+            console.log(snapshot.docs.map(doc => doc.data()))
+            setPosts(snapshot.docs.map(doc => doc.data()))
+        })
+
+    }, [])
+
     return (
         <div className='feed'>
             <div className='feed__input'>
                 <div className='feed__form'>
-                <Avatar />
-                <form>
-                    <input className='inputText' placeholder='Start a post' />
-                    <input type="submit" />
-                </form>
+                    <Avatar />
+                    <form onSubmit={submitPost}>
+                        <input className='inputText' placeholder='Start a post' value={input} onChange={e => setInput(e.target.value)} />
+                        <input type="submit" />
+                    </form>
                 </div>
-           <div className='feed__options'>
-                <div className="option">
-                    <PhotoIcon style={{
-                        color: '#70b5f9',
-                    }} />
-                    <span>Photo</span>
+                <div className='feed__options'>
+                    <div className="option">
+                        <PhotoIcon style={{
+                            color: '#70b5f9',
+                        }} />
+                        <span>Photo</span>
+                    </div>
+                    <div className="option">
+                        <YouTubeIcon style={{
+                            color: '#7fc15e',
+                        }} />
+                        <span>Video</span>
+                    </div>
+                    <div className="option">
+                        <TodayIcon style={{
+                            color: '#e7a33e',
+                        }} />
+                        <span>Event</span>
+                    </div>
+                    <div className="option">
+                        <ArticleIcon style={{
+                            color: '#fc9295',
+                        }} />
+                        <span>Write article</span>
+                    </div>
                 </div>
-                <div className="option">
-                    <YouTubeIcon style={{
-                        color: '#7fc15e',
-                    }}/>
-                    <span>Video</span>
-                </div>
-                <div className="option">
-                    <TodayIcon style={{
-                        color: '#e7a33e',
-                    }}/>
-                    <span>Event</span>
-                </div>
-                <div className="option">
-                    <ArticleIcon style={{
-                        color: '#fc9295',
-                    }}/>
-                    <span>Write article</span>
-                </div>
+
             </div>
-            
-        </div>
-        <Post name="Jishan Pathan" description="This is Test" message="Learning react js" photoUrl="https://media-exp1.licdn.com/dms/image/C4E03AQGgXeaxRZUVqA/profile-displayphoto-shrink_100_100/0/1640021234235?e=1651104000&v=beta&t=0ft1WiEhi_F0i-LKlI2kveAFJmpatNjEjcTmI_murME"/>
-        <Post name="Jishan Pathan" description="This is Test" message="Learning react js" photoUrl="https://media-exp1.licdn.com/dms/image/C4E03AQGgXeaxRZUVqA/profile-displayphoto-shrink_100_100/0/1640021234235?e=1651104000&v=beta&t=0ft1WiEhi_F0i-LKlI2kveAFJmpatNjEjcTmI_murME"/>
-        <Post name="Jishan Pathan" description="This is Test" message="Learning react js" photoUrl="https://media-exp1.licdn.com/dms/image/C4E03AQGgXeaxRZUVqA/profile-displayphoto-shrink_100_100/0/1640021234235?e=1651104000&v=beta&t=0ft1WiEhi_F0i-LKlI2kveAFJmpatNjEjcTmI_murME"/>
-        <Post name="Jishan Pathan" description="This is Test" message="Learning react js" photoUrl="https://media-exp1.licdn.com/dms/image/C4E03AQGgXeaxRZUVqA/profile-displayphoto-shrink_100_100/0/1640021234235?e=1651104000&v=beta&t=0ft1WiEhi_F0i-LKlI2kveAFJmpatNjEjcTmI_murME"/>
-      
+            <Post name="Jishan Pathan" description="This is Test" message="Learning react js" photoUrl="https://media-exp1.licdn.com/dms/image/C4E03AQGgXeaxRZUVqA/profile-displayphoto-shrink_100_100/0/1640021234235?e=1651104000&v=beta&t=0ft1WiEhi_F0i-LKlI2kveAFJmpatNjEjcTmI_murME" />
+
+
         </div>
     )
 }
---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/compat/app';
+
+import 'firebase/compat/firestore';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDb35UERbr7iyatmvHnIsNbq57koHBuMkI",
+    authDomain: "linkedin-clone-jp-5f7a2.firebaseapp.com",
+    projectId: "linkedin-clone-jp-5f7a2",
+    storageBucket: "linkedin-clone-jp-5f7a2.appspot.com",
+    messagingSenderId: "488387492198",
+    appId: "1:488387492198:web:62f500f312514443a370a7",
+    measurementId: "G-GZ04J4TWQC"
+  };
+
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+// Use these for db & auth
+const db = firebaseApp.firestore();
+
+
+export { db };
+
+   
